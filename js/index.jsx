b364db88b0592cf4b3d90bd0218b90b69270acdb
Sean Jun <seanjun21@gmail.com>
2016-07-21 12:06:57 -0700

    Added Components

--- js/index.jsx
+++ js/index.jsx
@@ -7,7 +7,17 @@ var Table = React.createClass( {
 
 } );
 
+var Hand = React.createClass( {
 
+} );
+
+var Button = React.createClass( {
+
+} );
+
+var Score = React.createClass( {
+
+} );
 
 document.addEventListener( 'DOMContentLoaded', function() {
   ReactDOM.render( <Table />, document.genElementByID( 'app' ) );
