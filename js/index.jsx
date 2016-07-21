0efe585466d26bfc9894eadb1f335b4e1efdbf33
Eric Snell <eric_snell21@yahoo.com>
2016-07-21 16:15:34 -0400

    Foundation commit

--- js/index.jsx
+++ js/index.jsx
@@ -3,22 +3,54 @@
 var React = require( 'react' );
 var ReactDOM = require( 'react-dom' );
 
-var Table = React.createClass( {
+ 
+var Game = React.createClass( {
+	// Callbacks/States Here
 
+
+	// Render Game
+	render: function() {
+		return (
+			<div className="table">
+				<div className="title">Lonely Black Jack</div>
+				<Hand />
+				<Sum />
+				<Button />
+			</div>
+		);
+	}
 } );
 
+
+// Generate two random numbers between 1 - 10 Math.floor(Math.random() * 10) + 1 
+// Holds button function to add another random number, concatenated
+/*-- Bust when number is greater than 21 --*/
 var Hand = React.createClass( {
+	var card1 = Math.floor(Math.random() * 10) + 1;
+	var card2 = Math.floor(Math.random() * 10) + 1;
 
+	render: function() {
+		return (
+
+			)
+	}
 } );
 
+
+// Upon clicking button, generates another random number added to value
 var Button = React.createClass( {
 
 } );
 
-var Score = React.createClass( {
+
+
+// Displays total of hands
+var Sum = React.createClass( {
 
 } );
 
+
+
 document.addEventListener( 'DOMContentLoaded', function() {
-  ReactDOM.render( <Table />, document.genElementByID( 'app' ) );
+  ReactDOM.render( <Game />, document.genElementByID( 'app' ) );
 } );
