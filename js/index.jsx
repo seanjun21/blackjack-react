823e247fddf2e651578b1c2e13c7ba9df4a9aa0b
Sean Jun <seanjun21@gmail.com>
2016-07-21 11:53:18 -0700

    Initial commit

---
+++ js/index.jsx
@@ -0,0 +1,14 @@
+// jshint ignore: start
+
+var React = require( 'react' );
+var ReactDOM = require( 'react-dom' );
+
+var Table = React.createClass( {
+
+} );
+
+
+
+document.addEventListener( 'DOMContentLoaded', function() {
+  ReactDOM.render( <Table />, document.genElementByID( 'app' ) );
+} );
