76d7f6d3e028fbba4f7a37749e0f1f106775c1aa
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-21 16:46:24 -0400

    Build Game component

--- js/index.jsx
+++ js/index.jsx
@@ -3,44 +3,61 @@
 var React = require( 'react' );
 var ReactDOM = require( 'react-dom' );
 
- 
+
 var Game = React.createClass( {
 	// Callbacks/States Here
 
-
 	// Render Game
 	render: function() {
+
+    	var randomCard = Math.floor(Math.random() * 10) + 1;
+      var randomCard2 = Math.floor(Math.random() * 10) + 1;
+
 		return (
 			<div className="table">
 				<div className="title">Lonely Black Jack</div>
-				<Hand />
-				<Sum />
-				<Button />
+				<Card text={randomCard}/>
+        <Card text={randomCard2}/>
+				{/*<Sum />
+				<Button />*/}
 			</div>
 		);
 	}
 } );
 
 
-// Generate two random numbers between 1 - 10 Math.floor(Math.random() * 10) + 1 
+// Generate two random numbers between 1 - 10 Math.floor(Math.random() * 10) + 1
 // Holds button function to add another random number, concatenated
 /*-- Bust when number is greater than 21 --*/
-var Hand = React.createClass( {
-	var card1 = Math.floor(Math.random() * 10) + 1;
-	var card2 = Math.floor(Math.random() * 10) + 1;
+var Card = React.createClass( {
+
+	// var card2 = Math.floor(Math.random() * 10) + 1;
 
 	render: function() {
-		return (
 
+		return (
+      <div>{this.props.text}</div>
 			)
 	}
 } );
 
 
 // Upon clicking button, generates another random number added to value
-var Button = React.createClass( {
 
-} );
+var Button = React.createClass ({
+  render: function() {
+    return (
+      <button
+        type="submit"
+        onClick={this.props.onClick}
+      />
+    );
+  }
+});
+
+
+
+
 
 
 
