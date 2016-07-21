735524bef89e80559b0cb57c0c47981328f08249
Alex Bang (MBP) <alexbang@hotmail.com>
2016-07-21 17:30:33 -0400

    Build card component

--- js/index.jsx
+++ js/index.jsx
@@ -6,20 +6,40 @@ var ReactDOM = require( 'react-dom' );
 
 var Game = React.createClass( {
 	// Callbacks/States Here
+  getInitialState: function() {
+
+    return {
+      initialCard: Math.floor(Math.random() * 10) + 1,
+      initialCard2: Math.floor(Math.random() * 10) + 1,
+      cardsArr: [],
+      cardNumb: ''
+    }
+  },
+
+  onClick: function() {
+
+    var randomCard = Math.floor(Math.random() * 10) + 1;
+
+    this.setState({
+        cardsArr: [randomCard]
+
+    })
+    console.log(this.state.cardsArr, "cardsArr");
+  },
+
 
 	// Render Game
 	render: function() {
 
-    	var randomCard = Math.floor(Math.random() * 10) + 1;
-      var randomCard2 = Math.floor(Math.random() * 10) + 1;
+
 
 		return (
 			<div className="table">
 				<div className="title">Lonely Black Jack</div>
-				<Card text={randomCard}/>
-        <Card text={randomCard2}/>
-				{/*<Sum />
-				<Button />*/}
+				<Card text={this.state.initialCard}/>
+        <Card text={this.state.initialCard2}/>
+        <div>{this.state.cardsArr}</div>
+				<Button onClick={this.onClick}/>
 			</div>
 		);
 	}
@@ -47,27 +67,21 @@ var Card = React.createClass( {
 var Button = React.createClass ({
   render: function() {
     return (
-      <button
-        type="submit"
-        onClick={this.props.onClick}
-      />
+      <button onClick={this.props.onClick} />
     );
   }
 });
 
-
-
-
-
-
-
-// Displays total of hands
-var Sum = React.createClass( {
-
-} );
+//
+//
+//
+// // Displays total of hands
+// var Sum = React.createClass( {
+//
+// } );
 
 
 
 document.addEventListener( 'DOMContentLoaded', function() {
-  ReactDOM.render( <Game />, document.genElementByID( 'app' ) );
+  ReactDOM.render( <Game />, document.getElementById( 'app' ) );
 } );
